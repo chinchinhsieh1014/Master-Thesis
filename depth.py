088f6b61e2f9104c8b5ab602a745f2b734610df1
YC,Hsieh <79919595+chinchinhsieh1014@users.noreply.github.com>
2022-08-08 01:22:45 +0100

    Add files via upload

--- depth.py
+++ depth.py
@@ -1,24 +1,28 @@
 #!/usr/bin/env python
-import rospy
-from sensor_msgs.msg import Image, CameraInfo
-from cv_bridge import CvBridge, CvBridgeError
-import cv2
-import pyrealsense2 as rs2
-import numpy as np
+import os
 import sys
+import cv2
+import open3d as o3d
+import rospy
+import numpy as np
+from sensor_msgs.msg import Image, CameraInfo, PointCloud2
+import sensor_msgs.point_cloud2 as pc2
+from std_msgs.msg import Header
+from cv_bridge import CvBridge, CvBridgeError
+import pyrealsense2 as rs2
 from realsense2_camera.msg import color_data
+import open3d as o3d
 
 class depth():
     def __init__(self):
-        rospy.Subscriber('color_data', color_data, self.callback)
+        rospy.Subscriber('color_data', color_data, self.getcorner)
         self.corner = []
         # Depth
         rospy.Subscriber("/camera/depth/camera_info", CameraInfo, self.depth_info)
         rospy.Subscriber("/camera/depth/image_rect_raw", Image, self.depth_callback)
         self.intrinsics = None
-        self.pix = None
 
-    def callback(self, data):
+    def getcorner(self, data):
         c = data.detected_corners
         c = list(c)
         np_array=np.asarray(c)
@@ -43,23 +47,12 @@ class depth():
         try:
             image_cv = bridge.imgmsg_to_cv2(data, desired_encoding='passthrough')
             # numpy.ndarray (720, 1280)
-
-            # pick one pixel among all the pixels with the closest range:
-            # indices = np.array(np.where(image_cv == image_cv[image_cv > 0].min()))[:,0]
-            # pix = (indices[1], indices[0])
-            # self.pix = pix
-            # line = '\rDepth at pixel(%3d, %3d): %7.1f(mm).' % (pix[0], pix[1], image_cv[pix[1], pix[0]])
-            # if self.intrinsics:
-            #     depth = image_cv[pix[1], pix[0]]
-            #     result = rs2.rs2_deproject_pixel_to_point(self.intrinsics, [pix[0], pix[1]], depth)
-            #     line += '  Coordinate: %8.2f %8.2f %8.2f.' % (result[0], result[1], result[2])
-            # sys.stdout.write(line)
-            # sys.stdout.flush()
         except CvBridgeError as e:
             rospy.logerr("CvBridge Error: {0}".format(e))
         # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
         depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(image_cv, alpha=0.05), cv2.COLORMAP_JET)
         # (720, 1280, 3)
+        box = []
         if len(self.corner)!=0:
             for i in self.corner:
                 x0 = min(i[0][0],i[1][0],i[2][0],i[3][0])
@@ -67,13 +60,36 @@ class depth():
                 x1 = max(i[0][0],i[1][0],i[2][0],i[3][0])
                 y1 = max(i[0][1],i[1][1],i[2][1],i[3][1])
                 cv2.rectangle(depth_colormap, (x0,y0), (x1,y1), (0,255,255), 3)
-                #cv2.rectangle(depth_colormap, (i[2][0],i[2][1]), (i[0][0],i[0][1]), (255,255,0), 1)
-        # Publish
+                box.append([[x0,y0],[x1,y1]])
+        # 2D Depth image to 3D point cloud
+        # [x] 1. get depth image from message
+        # [x] 2. calculate point cloud object(pcl) from depth image
+        # [x] 3. assign point cloud object to ROS PointCloud2 message
+        # [x] 4. Publish ROS message
+        # point cloud
+        if len(box)!=0:
+            pc_pub = rospy.Publisher('point_cloud', PointCloud2, queue_size=10)
+            #header
+            header = Header()
+            header.stamp = rospy.Time.now()
+            header.frame_id = 'map'
+            #cloud
+            points = []
+            for i in box: #[[x0,y0],[x1,y1]]
+                p = []
+                for u in range(i[0][0],i[1][0]):
+                    for v in range(i[0][1],i[1][1]):
+                        depth = image_cv[v, u]
+                        result = rs2.rs2_deproject_pixel_to_point(self.intrinsics, [u, v], depth)
+                        p.append([result[0],result[1],result[2]])
+            pointcloud = pc2.create_cloud_xyz32(header, points[0])
+            pc_pub.publish(pointcloud)
+
+        # frame
         pub = rospy.Publisher('image_depth_detected', Image, queue_size=10)
         msg_frame = CvBridge().cv2_to_imgmsg(depth_colormap, "bgr8")
         pub.publish(msg_frame)
         self.corner = []
-
 
 if __name__ == '__main__':
     # Initialize the node
