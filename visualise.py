d837061c8ae6e1304c4d07a2fb109ecdb313fded
YC,Hsieh <79919595+chinchinhsieh1014@users.noreply.github.com>
2022-08-12 00:27:08 +0100

    Add files via upload

--- visualise.py
+++ visualise.py
@@ -1,6 +1,6 @@
 import open3d as o3d
 import numpy as np
-pcd = o3d.io.read_point_cloud("/home/yc/catkin_ws/src/realsense-ros/realsense2_camera/dataset/ply/sense1/sense1_view1_17.ply")
+pcd = o3d.io.read_point_cloud("/home/yc/catkin_ws/src/realsense-ros/realsense2_camera/dataset/ply/sense1/sense1_view1_16.ply")
 print(pcd)
 print(np.asarray(pcd.points))
 o3d.visualization.draw_geometries([pcd])
