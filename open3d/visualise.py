d1d7d130a3187bb6cd3bc1199282a64a2aa76561
YC,Hsieh <79919595+chinchinhsieh1014@users.noreply.github.com>
2022-08-12 12:14:15 +0100

    Add files via upload

--- open3d/visualise.py
+++ open3d/visualise.py
@@ -1,14 +1,17 @@
 import open3d as o3d
 import numpy as np
-pcd = o3d.io.read_point_cloud("/home/yc/catkin_ws/src/realsense-ros/realsense2_camera/dataset/ply/sense1/sense1_view1_16.ply")
 
-# Visual point cloud
-#o3d.visualization.draw_geometries([pcd])
+pcd = o3d.io.read_point_cloud("/home/yc/catkin_ws/src/realsense-ros/realsense2_camera/dataset/ply/sense8/sense8_view7_37.ply")
+# Visualize
+# o3d.visualization.draw_geometries([pcd])
 
 # Surface normal
-downpcd = pcd.voxel_down_sample(0.01)
+downpcd = pcd.voxel_down_sample(0.5)
+# Visualize
 o3d.visualization.draw_geometries([downpcd])
-downpcd.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=0.01, max_nn=50))
-#radius: cm of search radius
-#max_nn: how many neighbors to save computation time
+
+downpcd.estimate_normals()
+
+#normals: Points normals.
+#print the first 1000 normals
 print(np.asarray(downpcd.normals)[:1000, :])
