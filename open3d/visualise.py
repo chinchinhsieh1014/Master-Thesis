0fd153c73094f3962415f8a308ea6c3e91e8775b
YC,Hsieh <79919595+chinchinhsieh1014@users.noreply.github.com>
2022-08-13 14:51:24 +0100

    Add files via upload

--- open3d/visualise.py
+++ open3d/visualise.py
@@ -1,17 +1,18 @@
 import open3d as o3d
 import numpy as np
+import pandas as pd
 
-pcd = o3d.io.read_point_cloud("/home/yc/catkin_ws/src/realsense-ros/realsense2_camera/dataset/ply/sense8/sense8_view7_37.ply")
+pcd = o3d.io.read_point_cloud("/home/yc/catkin_ws/src/realsense-ros/realsense2_camera/dataset/ply/sense1/sense1_view1_16.ply")
 # Visualize
 # o3d.visualization.draw_geometries([pcd])
 
 # Surface normal
 downpcd = pcd.voxel_down_sample(0.5)
 # Visualize
-o3d.visualization.draw_geometries([downpcd])
-
+#o3d.visualization.draw_geometries([downpcd])
 downpcd.estimate_normals()
 
 #normals: Points normals.
 #print the first 1000 normals
 print(np.asarray(downpcd.normals)[:1000, :])
+
